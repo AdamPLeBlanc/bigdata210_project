3da07c2e4e8dabcbef05a0c09532d57a05bca0ae
hecate007 <leblan7@gmail.com>
2016-12-06 22:03:36 -0800

    created csv file with zip to school district population data

--- leblanc_sandbox/test.py
+++ leblanc_sandbox/test.py
@@ -6,18 +6,25 @@ from pyspark.sql import HiveContext
 import os
 import collections
 
+def blah(row):
+    row[0] = row[0].replace(" School District",'')
+    return row
 
-dropoutRateCsvPath = r"../data/High_School_Dropout_Statistics_by_County_2012-2013_School_Year_5-Year_Cohort_Dropout_Rates.csv"
-enrollmentCsvPath = r"../data/K-12_Public_School_Enrollment_by_Grade_Level_October_Enrollment_Report_2009-2013.csv"
 schoolDistrictPopCsvPath = r"../data/WAOFM_-_SAEP_-_School_District_Population_Estimates__2000-2016.csv"
+zipToCityCsvPath = r"../data/zip_code_database.csv"
 
 #load school district population data
 parseCsv = lambda line : line.split(',')
 sc = SparkContext()
-dropoutRdd = sc.textFile(dropoutRateCsvPath).map(parseCsv)
-enrollmentRdd = sc.textFile(enrollmentCsvPath).map(parseCsv)
-popRdd = sc.textFile(schoolDistrictPopCsvPath).map(parseCsv)
-print dropoutRdd.take(5)
-print enrollmentRdd.take(5)
-print popRdd.take(5)
-print "done"
+popRdd = sc.textFile(schoolDistrictPopCsvPath).map(parseCsv).map(lambda line : [line[0],line[4:-5]]).map(blah)
+cityToZipRdd = sc.textFile(zipToCityCsvPath).map(parseCsv).map(lambda line : (line[3],line[0]))
+zipToPopRdd = cityToZipRdd.join(popRdd).map(lambda line : [line[1][0]]+line[1][1])
+'''zipToPopRdd.saveAsTextFile("zipToSchoolDistrictPop")
+from fileinput import input
+from glob import glob
+open("zipToSchoolDistrictPop.csv","wb").write(''.join(sorted(input(glob("zipToSchoolDistrictPop/part-0000*")))))'''
+print popRdd.take(1)
+print cityToZipRdd.take(1)
+print zipToPopRdd.take(1)
+
+
