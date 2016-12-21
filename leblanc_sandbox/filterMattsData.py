5079555fda2e550bfa681097fbad448728cab087
hecate007 <leblan7@gmail.com>
2016-12-20 23:59:02 -0800

    updated project pyspark script

--- leblanc_sandbox/filterMattsData.py
+++ leblanc_sandbox/filterMattsData.py
@@ -2,19 +2,33 @@
 import os
 from pyspark import SparkConf, SparkContext
 
-def blah(row):
-    row[0] = row[0].replace(" School District",'')
-    return row
 
+zillowDataPath = r"../data/Zip_Zhvi_AllHomes_Historical.csv"
+zipCodeDataPath = r"../data/zip_code_database.csv"
+schoolDistrictDataPath = r"../data/WAOFM_-_SAEP_-_School_District_Population_Estimates__2000-2016.csv"
+demoDataPath = r"../data/14zpallnoagi.csv"
 
-dataPath = r"../data/Zip_Zhvi_AllHomes_Historical.csv"
-
-#load school district population data
-parseCsv = lambda line : line.split(',')
+#create a spark context
 sc = SparkContext()
-origRdd = sc.textFile(dataPath).map(parseCsv).filter(lambda line : line[3] == "WA").map(lambda line : line[:6])
-print "sample size: {0}".format(origRdd.count())
-for sample in origRdd.take(100):
+
+#load zipcode data and just save primary city and zip code . column 3 & 0
+zipRdd = sc.textFile(zipCodeDataPath).map(lambda line : line.split(',')).map(lambda row : [row[3],row[0]])
+
+#load school district population data, stripping "school district" from the name and saving the numeric & percent change from 2010-2016
+sdRdd = sc.textFile(schoolDistrictDataPath).map(lambda line : line.split(':')).map(lambda row : [row[0].replace(" School District",''),row[-5:-1]])
+
+#load housing data and grab city and size rank
+housingRdd = sc.textFile(zillowDataPath).map(lambda line : line.split(',')).filter(lambda row : row[3] == "WA").map(lambda row : [row[2],row[6]])
+
+#load demographic data
+demoRdd = sc.textFile(demoDataPath).map(lambda line : line.split(',')).map(lambda row : [row[2],row[14]])
+
+finalRdd = zipRdd.join(sdRdd).map(lambda row : [row[0],[row[1][0]] + row[1][1]])
+finalRdd = finalRdd.join(housingRdd).map(lambda row : [row[1][0][0],[row[0]]+row[1][0][1:]+[row[1][1]]])
+finalRdd = finalRdd.join(demoRdd)
+
+print "sample size: {0}".format(finalRdd.count())
+for sample in finalRdd.take(100):
     print sample
 '''zipToPopRdd.saveAsTextFile("zipToSchoolDistrictPop")
 from fileinput import input
